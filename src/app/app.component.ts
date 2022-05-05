8a14c0bfd5be6485cbc1f600b30cca6e73b7a214
Roman Udalov <Udalovrom52@gmail.com>
2022-05-05 14:47:11 +0300

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { loadAuth } from './shared/state/auth.actions';
 import { AppState } from './store/app.state';
@@ -9,7 +10,13 @@ import { AppState } from './store/app.state';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private router: Router) {
+    let path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
   ngOnInit(): void {
     this.store.dispatch(loadAuth())
   }
